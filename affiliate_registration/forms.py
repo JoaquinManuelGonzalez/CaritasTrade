8f9029a1c9dca1e7a0efec6943397912cdd4550f
JOAQUíN MANUEL GONZALEZ <joaquinmgonzalez16@outlook.com>
2024-05-08 18:32:27 -0300

    fallo verificaciones len

--- affiliate_registration/forms.py
+++ affiliate_registration/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 class Register_Form(forms.Form):
     dni = forms.CharField(label="dni", max_length=8)
@@ -8,4 +9,31 @@ class Register_Form(forms.Form):
     phone_number = forms.CharField(label="phone_number", max_length=10)
     birth_day = forms.DateField(label="birth_day")
     password = forms.CharField(label="password", max_length=8)
- 
+
+    def clean_name(self):
+        new_name = self.cleaned_data.get('name')
+        if (len(new_name) > 20):
+            raise ValidationError(
+                "El/Los Nombre/s ingresado es inválido. Tamaño erróneo.")
+        return new_name
+
+    def clean_surname(self):
+        new_surname = self.cleaned_data.get('surname')
+        if (len(new_surname) > 20):
+            raise ValidationError(
+                "El/Los Apellido/s ingresado es inválido. Tamaño erróneo.")
+        return new_surname
+
+    def clean_phone_number(self):
+        new_phone_number = self.cleaned_data.get('phone_number')
+        if (len(new_phone_number) > 10):
+            raise ValidationError(
+                "El Número Telefónico ingresado es inválido. Tamaño erróneo.")
+        return new_phone_number
+
+    def clean_dni(self):
+        new_dni = self.cleaned_data.get('dni')
+        if (len(new_dni) != 8):
+            raise ValidationError(
+                "El D.N.I ingresado es inválido. Tamaño erróneo.")
+        return new_dni
--- affiliate_registration/views.py
+++ affiliate_registration/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 from django.core.exceptions import ValidationError
 from datetime import date
@@ -8,14 +9,14 @@ from . import forms
 from data_base.models import Affiliate, Reputation
 
 
-def exist_email(new_email):
+def check_email(new_email):
     if Affiliate.objects.filter(email=new_email).exists():
         raise ValidationError(
             "El email ingresado ya se encuentra registrado en el sistema.")
 
 
-def exist_dni(new_dni):
-    if Affiliate.objects.filter(dni=new_dni).exists():
+def check_dni(new_dni):
+    if(Affiliate.objects.filter(dni=new_dni).exists()):
         raise ValidationError(
             "El D.N.I ingresado ya se encuentra registrado en el sistema.")
 
@@ -28,7 +29,7 @@ def check_birth_day(new_birth_day):
 
 
 def check_phone_number(new_phone_number):
-    if not new_phone_number.startswith("221"):
+    if (len(new_phone_number) > 10) or (not new_phone_number.startswith("221")):
         raise ValidationError(
             "El número telefónico ingresado es inválido. Debe ser un número telefónico proveniente de La Plata")
 
@@ -36,7 +37,7 @@ def check_phone_number(new_phone_number):
 def check_password(new_password):
     if (len(new_password) < 8) or (not re.search(r"[a-zA-Z0-9\W_]", new_password)):
         raise ValidationError(
-            "La contraseña ingresada es inválida. Debe contener 8 caracteres como mínimo y contar con la combinación de números, letras y caracteres especiales.")
+            "La contraseña ingresada es inválida. Debe contener 8 caracteres y contar con la combinación de números, letras y caracteres especiales.")
 
 
 def registration_form(request):
@@ -44,10 +45,14 @@ def registration_form(request):
         registration_form = forms.Register_Form(request.POST)
         if registration_form.is_valid():
             try:
+                registration_form.clean_dni()
+                registration_form.clean_name()
+                registration_form.clean_phone_number()
+                registration_form.clean_surname()           
                 dni = registration_form.cleaned_data["dni"]
-                exist_dni(dni)
+                check_dni(dni)
                 email = registration_form.cleaned_data["email"]
-                exist_email(email)
+                check_email(email)
                 birth_day = registration_form.cleaned_data["birth_day"]
                 check_birth_day(birth_day)
                 phone_number = registration_form.cleaned_data["phone_number"]
@@ -63,17 +68,17 @@ def registration_form(request):
             new_affiliate = Affiliate.objects.create(
                 dni=request.POST["dni"],
                 email=request.POST["email"],
-                name=request.POST["name"],
-                surname=request.POST["surname"],
+                name=request.POST["name"].lower().capitalize(),
+                surname=request.POST["surname"].lower().capitalize(),
                 phone_number=request.POST["phone_number"],
                 birth_day=request.POST["birth_day"],
                 password=request.POST["password"],
                 reputation_id=new_reputation
             )
-            return HttpResponseRedirect("/landing_page/")
+            return HttpResponseRedirect(reverse("landing_page"))
         else:
             return render(request, "registration_form.html", {
-                "form": registration_form
+                "form" :registration_form
             })
     else:
         registration_form = forms.Register_Form()
