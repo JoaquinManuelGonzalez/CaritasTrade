8990e1b91dee48fb8d84d6eb26f205b16fa20932
Francisco Percara <99842611+Kelisei@users.noreply.github.com>
2024-05-08 23:36:42 -0300

    Que la pasa a la imagen 🦅🐍

--- caritasTrade/settings.py
+++ caritasTrade/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'caritasTrade.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.dirname(__file__), 'templates_share'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- create_post/views.py
+++ create_post/views.py
@@ -1,27 +1,23 @@
-from datetime import date
+import datetime
 from django.shortcuts import redirect, render
-from data_base.models import ExchangePost
-
+from data_base.models import ExchangePost, ProductCategory
+from .forms import ExchangeForm
 # Create your views here.
 def create_post(request):    
     if request.method == 'GET':# and request.session.get("id"):
         if not request.session.get("id"):
            request.session["id"] = 1
-        categories=['Utiles']
+        form = ExchangeForm()
         return render(request, 'create_post.html', {
-            'categories': 
+            'categories': ProductCategory.objects.all(),
+            "form": form,
         })
     elif request.method == 'POST':# and request.sesssion.get("id"):
-        post = ExchangePost(
-            affiliate_id = request.session.get("id"),
-            product_category_id=request.POST['product_category_id'],
-            title=request.POST['title'],
-            image=request.POST['image'],
-            description=request.POST['description'],
-            timestamp= date.today(),
-            is_active=True
-        )
-        post.save()
+        form = ExchangeForm(request.POST)
+        if form.is_valid():
+            print("Es valido caralho")
+        else:
+            print(form.errors)
         return redirect('landing_page')
     else:
         return redirect('landing_page')
