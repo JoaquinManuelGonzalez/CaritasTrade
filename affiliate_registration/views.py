b301e38f0f92887ea8323dbe162b9965518b783b
JOAQUíN MANUEL GONZALEZ <joaquinmgonzalez16@outlook.com>
2024-05-09 18:25:31 -0300

    registro final... por ahora

--- affiliate_registration/views.py
+++ affiliate_registration/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 from . import forms
 from data_base.models import Affiliate, Reputation
 
+def capitalize_element(data):
+    data_list = data.split()
+    data_list_capitalized = [data.lower().capitalize() for data in data_list]
+    data_capitalized = ' '.join(data_list_capitalized)
+    return data_capitalized
 
 def registration_form(request):
     if request.method == "POST":
@@ -12,7 +17,9 @@ def registration_form(request):
             dni = registration_form.cleaned_data['dni']
             email = registration_form.cleaned_data['email']
             name = registration_form.cleaned_data['name']
+            name_capitalized = capitalize_element(name)
             surname = registration_form.cleaned_data['surname']
+            surname_capitalized = capitalize_element(surname)
             phone_number = registration_form.cleaned_data['phone_number']
             birth_day = registration_form.cleaned_data['birth_day']
             password = registration_form.cleaned_data['password']
@@ -20,14 +27,14 @@ def registration_form(request):
             new_affiliate = Affiliate.objects.create(
                 dni=dni,
                 email=email,
-                name=name.lower().capitalize(),
-                surname=surname.lower().capitalize(),
+                name=name_capitalized,
+                surname=surname_capitalized,
                 phone_number=phone_number,
                 birth_day=birth_day,
                 password=password,
                 reputation_id=new_reputation
             )
-            return HttpResponseRedirect(reverse("log_in"))
+            return render(request, "success_message.html")
         else:
             return render(request, "registration_form.html", {
                 "form" :registration_form
