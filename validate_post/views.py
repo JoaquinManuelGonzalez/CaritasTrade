21c07f4364498a488651283419c2dd36cded7dd9
Francisco Percara <frankestudios71@gmail.com>
2024-05-15 10:55:44 -0300

    Chequeo segun la HU 🚨🚨🚨

--- validate_post/views.py
+++ validate_post/views.py
@@ -58,12 +58,29 @@ def send_admin_email(email):
 
 def accept(request):
     if request.method == 'POST':
+        success_message_accept = None 
+        failure_message_accept = None
         post_id = request.POST.get("id")
         post = ExchangePost.objects.filter(id=post_id).first()
-        post.is_active = True
-        post.save()
-        affiliate = post.affiliate
-        send_accept_email(affiliate.email)
+        if ExchangePost.objects.filter(affiliate_id=post.affiliate_id, is_active=True).count() >= 5:
+            failure_message_accept = "No puedes tener mas de 5 publicaciones activas"
+        else:
+            post.is_active = True
+            post.save()
+            affiliate = post.affiliate
+            send_accept_email(affiliate.email)
+            success_message_accept = "Publicacion aceptada"
+        #Renderizo la pagina devuelta con un mensaje.
+        blocked_affiliates = AccountBlock.objects.values_list('affiliate_id', flat=True)
+        posts = ExchangePost.objects.filter(is_active=False, is_rejected=False).exclude(affiliate_id__in=blocked_affiliates)
+        post_info = []
+        for post in posts:
+            aux = {"id": post.id, "title": post.title, "description": post.description, "category": post.product_category.name, "image": None, "attempts": post.affiliate.rejected_posts}
+            if post.image:
+                image_data = post.image.decode("utf-8")
+                aux["image"] = image_data
+            post_info.append(aux)
+        return render(request, "see_waiting_posts.html", {"posts": post_info, "role": request.session.get("role"), "failure_message_accept": failure_message_accept, "success_message_accept": success_message_accept})
     return redirect("see_waiting_posts")
 
 def reject(request):
