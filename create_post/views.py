0569b9968d126fec1a5be5b67ad05babae3e073f
Francisco Percara <99842611+Kelisei@users.noreply.github.com>
2024-05-23 20:12:30 -0300

    Refactorizacion de create_post

--- create_post/views.py
+++ create_post/views.py
@@ -8,67 +8,48 @@ from view_profile.views import session_name
 
 # Create your views here.
 def create_post(request):
-
-    if request.method == "GET" and request.session.get("id"):
-        form = ExchangeForm()
-        return render(
-            request,
-            "create_post.html",
-            {
-                "categories": ProductCategory.objects.all(),
-                "form": form,
-                "user_session": False,
-                "session_id": request.session.get("id"),
-                "session_name": session_name(request),
-            },
-        )
-    elif request.method == "POST" and request.session.get("id"):
-        form = ExchangeForm(request.POST, request.FILES)
-        affiliate = Affiliate.objects.get(id=request.session.get("id"))
-        num_posts = ExchangePost.objects.filter(
-            affiliate_id=affiliate, is_rejected=False, is_active=True
-        ).count()
-        if form.is_valid() and num_posts < 5:
-            post = ExchangePost(
-                title=form.cleaned_data["title"],
-                product_category_id=form.cleaned_data["category"].id,
-                description=form.cleaned_data["description"],
-                image=(
-                    base64.b64encode(form.cleaned_data["image"].read())
-                    if form.cleaned_data["image"]
-                    else form.cleaned_data["image"]
-                ),
-                timestamp=datetime.datetime.now(),
-                is_active=False,
-                is_rejected=False,
-                affiliate_id=request.session["id"],
-            )
-            post.save()
-            return render(
-                request,
-                "create_post.html",
-                {
-                    "form": form,
-                    "categories": ProductCategory.objects.all(),
-                    "success_message": "La publicación que creaste está ahora en estado pendiente. Nuestro equipo de trabajo la revisará y te notificará vía mail si fue aprobada o no.",
-                    "user_session": False,
-                    "session_id": request.session.get("id"),
-                    "session_name": session_name(request),
-                },
-            )
-        else:
-            if not form.errors and num_posts >= 5:
-                form.add_error(None, "Limite de publicaciones alcanzado")
-            return render(
-                request,
-                "create_post.html",
-                {
-                    "form": form,
-                    "categories": ProductCategory.objects.all(),
-                    "user_session": False,
-                    "session_id": request.session.get("id"),
-                    "session_name": session_name(request),
-                },
-            )
-    else:
+    if not request.session.get("id"):
         return redirect("landing_page")
+
+    form = ExchangeForm(request.POST or None, request.FILES or None)
+    affiliate = Affiliate.objects.get(id=request.session.get("id"))
+    num_posts = ExchangePost.objects.filter(
+        affiliate_id=affiliate, is_rejected=False, is_active=True
+    ).count()
+
+    if form.is_valid() and num_posts < 5:
+        post = ExchangePost(
+            title=form.cleaned_data["title"],
+            product_category_id=form.cleaned_data["category"].id,
+            description=form.cleaned_data["description"],
+            image=(
+                base64.b64encode(form.cleaned_data["image"].read())
+                if form.cleaned_data["image"]
+                else form.cleaned_data["image"]
+            ),
+            timestamp=datetime.datetime.now(),
+            is_active=False,
+            is_rejected=False,
+            affiliate_id=request.session["id"],
+        )
+        post.save()
+        form = ExchangeForm()
+        success_message = "La publicación que creaste está ahora en estado pendiente. Nuestro equipo de trabajo la revisará y te notificará vía mail si fue aprobada o no."
+    else:
+        if not form.errors and num_posts >= 5:
+            form.add_error(None, "Limite de publicaciones alcanzado")
+
+    return render(
+        request,
+        "create_post.html",
+        {
+            "form": form,
+            "categories": ProductCategory.objects.all(),
+            "success_message": (
+                success_message if form.is_valid() and num_posts < 5 else None
+            ),
+            "user_session": False,
+            "session_id": request.session.get("id"),
+            "session_name": session_name(request),
+        },
+    )
